55bbd99f60d6653c527f6425b5ee37e7d2d79f52
Koen Van Daele <koen_van_daele@telenet.be>
2012-01-12 08:31:53 +0100

    Nog meer unit tests voor de crabgateway.

--- test/tests/KVD/Services/Agiv/Crab/CrabGatewayTest.php
+++ test/tests/KVD/Services/Agiv/Crab/CrabGatewayTest.php
@@ -144,6 +144,66 @@ class CaPaKeyGatewayTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals( 'Nummer van een appartement.', $first->getDefinitie( ) );
     }
 
+    public function testListAardTerreinobjecten( )
+    {
+        $client = $this->getMockBuilder('KVD\Services\Agiv\Crab\SoapClient')
+                       ->disableOriginalConstructor( )
+                       ->setMethods( array( 'ListAardTerreinobjecten') )
+                       ->getMock( );
+        $gateway = new CrabGateway( $client );
+
+        $aarden = array();
+        $a = new \StdClass( );
+        $a->Code = 4;
+        $a->Naam = 'grbAdmPerceel';
+        $a->Definitie = 'Een perceel volgens het GRB.';
+        $aarden[] = $a;
+        $res = new \StdClass( );
+        $res->ListAardTerreinobjectenResult = new \StdClass( );
+        $res->ListAardTerreinobjectenResult->CodeItem = $aarden;
+        $client->expects( $this->once( ) )
+               ->method( 'ListAardTerreinobjecten' )
+               ->will( $this->returnValue( $res ) );
+        $resultaat = $gateway->listAardTerreinobjecten();
+        $this->assertInternalType( 'array', $resultaat );
+        $this->assertEquals( 1, count( $resultaat ) );
+        $first = $resultaat[0];
+        $this->assertInstanceOf( 'KVD\Services\Agiv\Crab\AardTerreinobject', $first );
+        $this->assertEquals( 4, $first->getCode( ) );
+        $this->assertEquals( 'grbAdmPerceel', $first->getNaam( ) );
+        $this->assertEquals( 'Een perceel volgens het GRB.', $first->getDefinitie( ) );
+    }
+
+    public function testListAardWegobjecten( )
+    {
+        $client = $this->getMockBuilder('KVD\Services\Agiv\Crab\SoapClient')
+                       ->disableOriginalConstructor( )
+                       ->setMethods( array( 'ListAardWegobjecten') )
+                       ->getMock( );
+        $gateway = new CrabGateway( $client );
+
+        $aarden = array();
+        $a = new \StdClass( );
+        $a->Code = 2;
+        $a->Naam = 'grbWegverbinding';
+        $a->Definitie = 'Een wegverbinding volgens het GRB.';
+        $aarden[] = $a;
+        $res = new \StdClass( );
+        $res->ListAardWegobjectenResult = new \StdClass( );
+        $res->ListAardWegobjectenResult->CodeItem = $aarden;
+        $client->expects( $this->once( ) )
+               ->method( 'ListAardWegobjecten' )
+               ->will( $this->returnValue( $res ) );
+        $resultaat = $gateway->listAardWegobjecten();
+        $this->assertInternalType( 'array', $resultaat );
+        $this->assertEquals( 1, count( $resultaat ) );
+        $first = $resultaat[0];
+        $this->assertInstanceOf( 'KVD\Services\Agiv\Crab\AardWegobject', $first );
+        $this->assertEquals( 2, $first->getCode( ) );
+        $this->assertEquals( 'grbWegverbinding', $first->getNaam( ) );
+        $this->assertEquals( 'Een wegverbinding volgens het GRB.', $first->getDefinitie( ) );
+    }
+
     public function testListGemeentenByGewestId( )
     {
         $client = $this->getMockBuilder('KVD\Services\Agiv\Crab\SoapClient')
@@ -281,6 +341,93 @@ class CaPaKeyGatewayTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf( 'KVD\Services\Agiv\Crab\Gemeente', $resultaat );
         $this->assertEquals( 666, $resultaat->getId( ) );
         $this->assertEquals( 'TestGemeente', $resultaat->getNaam( ) );
+    }
+
+    public function testGetGemeenteByNaam( )
+    {
+        $client = $this->getMockBuilder('KVD\Services\Agiv\Crab\SoapClient')
+                       ->disableOriginalConstructor( )
+                       ->setMethods( array( 'GetGemeenteByGemeenteNaam') )
+                       ->getMock( );
+        $gateway = new CrabGateway( $client );
+
+        $p = new \StdClass();
+        $p->GemeenteNaam = 'TestGemeente';
+        $p->GewestId = 2; 
+        $pWrapper = new \SoapParam ( $p , "GetGemeenteByGemeenteNaam" );
+        $gem = new \StdClass( );
+        $gem->GemeenteId = 666;
+        $gem->NisGemeenteCode = 39999;
+        $gem->GemeenteNaam = 'TestGemeente';
+        $gem->TaalCodeGemeenteNaam = 'nl';
+        $gem->TaalCode = 'nl';
+        $gem->CenterX = 10.0;
+        $gem->CenterY = 10.0;
+        $gem->MinimumX = 0.0;
+        $gem->MinimumY = 0.0;
+        $gem->MaximumX = 20.0;
+        $gem->MaximumY = 20.0;
+        $res = new \StdClass( );
+        $res->GetGemeenteByGemeenteNaamResult = $gem;
+        $client->expects( $this->once( ) )
+               ->method( 'GetGemeenteByGemeenteNaam' )
+               ->with( $pWrapper )
+               ->will( $this->returnValue( $res ) );
+        $resultaat = $gateway->getGemeenteByNaam( 'TestGemeente' );
+        $this->assertInstanceOf( 'KVD\Services\Agiv\Crab\Gemeente', $resultaat );
+        $this->assertEquals( 666, $resultaat->getId( ) );
+        $this->assertEquals( 'TestGemeente', $resultaat->getNaam( ) );
+    }
+
+    public function testListStratenByGemeente( )
+    {
+        $client = $this->getMockBuilder('KVD\Services\Agiv\Crab\SoapClient')
+                       ->disableOriginalConstructor( )
+                       ->setMethods( array( 'ListStraatnamenByGemeenteId') )
+                       ->getMock( );
+        $gateway = new CrabGateway( $client );
+
+        $p = new \StdClass();
+        $p->GemeenteId = 666;
+        $p->SorteerVeld = 6; 
+        $pWrapper = new \SoapParam ( $p , "ListStraatnamenByGemeenteId" );
+        $straten = array();
+        $ts = new \StdClass( );
+        $ts->StraatnaamId = 456;
+        $ts->StraatnaamLabel = 'Teststraat';
+        $ts->TaalCode = 'nl';
+        $ts->Straatnaam = 'Teststraat';
+        $ts->TaalCodeTweedeTaal = 'fr';
+        $ts->StraatnaamTweedeTaal = 'Rue du Test';
+        $straten[] = $ts;
+        $ts = new \StdClass( );
+        $ts->StraatnaamId = 4555;
+        $ts->StraatnaamLabel = 'Avenue';
+        $ts->TaalCode = 'nl';
+        $ts->Straatnaam = 'Avenue';
+        $ts->TaalCodeTweedeTaal = 'fr';
+        $straten[] = $ts;
+        $res = new \StdClass( );
+        $res->ListStraatnamenByGemeenteIdResult = new \StdClass( );
+        $res->ListStraatnamenByGemeenteIdResult->StraatnaamItem = $straten;
+        $client->expects( $this->once( ) )
+               ->method( 'ListStraatnamenByGemeenteId' )
+               ->with( $pWrapper )
+               ->will( $this->returnValue( $res ) );
+        $gemeente = new Gemeente( 666, 39999, array( 'nl' => 'TestGemeente' ), 'nl' );
+        $resultaat = $gateway->listStratenByGemeente( $gemeente );
+        $this->assertInternalType( 'array', $resultaat );
+        $this->assertEquals( 2, count( $resultaat ) );
+        $first = $resultaat[0];
+        $this->assertInstanceOf( 'KVD\Services\Agiv\Crab\Straat', $first );
+        $this->assertInstanceOf( 'KVD\Services\Agiv\Crab\Gemeente', $first->getGemeente( ) );
+        $this->assertEquals( 456, $first->getId( ) );
+        $this->assertEquals( $gemeente->getId( ), $first->getGemeente( )->getId( ) );
+        $this->assertEquals( $gemeente->getNaam( ), $first->getGemeente( )->getNaam( ) );
+        $this->assertEquals( 'Teststraat', $first->getNaam( ) );
+        $this->assertEquals( 'Rue du Test', $first->getNaam( 'fr' ) );
+        $second = $resultaat[1];
+        $this->assertEquals( $second->getNaam( 'nl' ), $second->getNaam( 'fr' ) );
     }
 }
 ?>
